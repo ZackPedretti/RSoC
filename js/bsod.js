bdc78dc5f3cf0fe324cfd4f35beb4055ea98b108
ZackPedretti <zackpedretti52@gmail.com>
2025-10-23 11:56:51 +0200

    feat: style and text

--- js/bsod.js
+++ js/bsod.js
@@ -1,8 +1,8 @@
 var qrcode = new QRCode(document.getElementById("qrcode"), {
-	text: "https://beta.amanotem.com",
+	text: "https://au-bon-pain.fr/",
 	width: 110,
 	height: 110,
-	colorDark : "#106faa",
+	colorDark : "#9E1B32",
 	colorLight : "#ffffff",
 	correctLevel : QRCode.CorrectLevel.H
 });
